14e7ca41acc65d40e28ec18b97e54fcba5f146ee
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-03-25 17:05:16 -0300

    feature: final application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import './global.css'
+
+import Routes from './routes'
+
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <> 
+      <Routes />
+    </>
   );
 }
 
